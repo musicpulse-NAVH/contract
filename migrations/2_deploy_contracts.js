9c3380fa300c75ed6cb27c6493d67775b6a4de11
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2023-08-16 15:16:26 -0400

    add ERC6551

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const MusicPulseNFT = artifacts.require("MusicPulseNFT");
+const ERC6551Account = artifacts.require("ERC6551Account");
+const ERC6551Registry = artifacts.require("ERC6551Registry");
 
 module.exports = async function(deployer){
     await deployer.deploy(MusicPulseNFT);
+    await deployer.deploy(ERC6551Account);
+    await deployer.deploy(ERC6551Registry);
 };
