59424c311d3ffad0c919ef1d3382bcb947c4e0df
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2023-08-19 22:12:51 -0400

    fix the contract

--- contracts/AccessToken.sol
+++ contracts/AccessToken.sol
@@ -13,21 +13,6 @@ contract AccessToken is ERC20, Ownable {
         _mint(account, 1 * 10 ** 18);
     }
 
-    // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract AccessToken is ERC20, Ownable {
-    mapping(address => bool) private _soulbound;
-
-    constructor() payable ERC20("Access Token", "AT") {}
-
-    function mint(address account) public {
-        _mint(account, 1 * 10 ** 18);
-    }
-
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         revert("Transfers are disabled");
     }
@@ -39,5 +24,4 @@ contract AccessToken is ERC20, Ownable {
     ) public override returns (bool) {
         revert("Transfers are disabled");
     }
-}
 }
